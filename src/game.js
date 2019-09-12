7423ba57113bdeff861962070e49a48f669e5ff8
dreamblader <gm3_gab@hotmail.com>
2019-09-12 13:51:02 -0300

    game taking shape

--- src/game.js
+++ src/game.js
@@ -1,18 +1,26 @@
 kontra.init();
 kontra.initKeys();
 
+var loop;
+var isLoaded = false;
+var obstacles = [];
 var canvas = document.getElementById("screen");
+var score_html = document.getElementById("score");
+var text_box = document.getElementById("text-box");
 var width= canvas.width;
 var height= canvas.height;
+var score = 0;
+var time = 0;
+var spawn_period = 300;
 
 kontra.setImagePath('assets/images');
 
-kontra.load('a.png').then(function()
+kontra.load('a.png', 'tomato.png').then(function()
 {
 
     var runner_sprite = kontra.Sprite(
         {
-            x: (width/2),
+            x: (width/2)-8,
             y: height-50,
             image: kontra.imageAssets.a
         });
@@ -27,12 +35,19 @@ kontra.load('a.png').then(function()
         });
         
     // global mechanic variables
-    var y_origin = runner_sprite.y;
+    y_origin = runner_sprite.y;
+    spawner_ground = y_origin;
+    spawner_min = y_origin-8;
+    spawner_med = y_origin-16;
+    spawner_max = y_origin-24;
         
-        var loop = kontra.GameLoop(
+        loop = kontra.GameLoop(
         {
             update: function()
             {
+
+                time++;
+
                 if(kontra.keyPressed('up')) // JUMP
                 {
                     jump(runner_sprite);                   
@@ -42,44 +57,90 @@ kontra.load('a.png').then(function()
                     stop_jump();
                 }
 
+
+                /*
+                //TODO:FIXME
                 if(kontra.keyPressed('down')) // SLIDE
                 {
-                    console.log(down_score);
-                    down_score++;
-
-                    if(down_score <20)
-                    {
-                        runner_sprite.y = y_origin + 5;
-                    }
-                    else
-                    {
-                        runner_sprite.y = y_origin;
-                    }
+                    
+                   slide(runner_sprite);
                 }
                 else
                 {
                     //runner_sprite.y = y_origin;
                     down_score = 0;
                 }
+                */
 
                 
-                gravity(runner_sprite);
-                
 
+                if(runner_sprite.y < y_origin) //GRAVITY
+                {
+                    gravity(runner_sprite);
+                }
+                else if(runner_sprite.y == y_origin) //TOUCHED GROUND
+                {
+                    jump_cooloff();
+                }
+
+                if(time%spawn_period == 0)
+                {
+                    score+=5;
+                    var obstacle = spawn_obstacle();
+                    obstacles.push(obstacle);
+                }
+                
                 runner_sprite.update();
                 ground.update();
+
+                obstacles.forEach(function(obstacle) 
+                {
+                    obstacle.update();
+
+                    if(obstacle.collidesWith(runner_sprite))
+                    {
+                        loop.stop();
+                        text_box.innerHTML = "DEAD!";
+                    }
+                });
+
+                score_html.innerHTML = score;
             },
         
             render: function()
-            {
+            {  
                 ground.render();
                 runner_sprite.render();
+                if(obstacles.length > 0)
+                {
+                    for(i=0;i<obstacles.length;i++)
+                    obstacles[i].render();
+                    //obstacles.array.forEach(function(obstacle) 
+                    //{
+                    //    obstacle.render();
+                    //});
+                }
             }
         });
         
         
         
-        loop.start();
+        
+        //}
+        isLoaded = true;
 });
+
+
+///////////////////////////////////////////////// TODO
+document.onkeypress = start_game();
+
+function start_game()
+{
+    console.log("whaaat");
+    if(isLoaded)
+    {
+        loop.start();
+    }
+}
 
 
--- src/mechanics.js
+++ src/mechanics.js
@@ -10,6 +10,13 @@ var down_score = 0;
 var max_up = 15;
 var max_down = 20;
 
+var spawner_ground;
+var spawner_min;
+var spawner_med;
+var spawner_max;
+
+var game_speed = 1;
+
 function jump(sprite)
 {
     console.log(up_score);
@@ -26,14 +33,71 @@ function stop_jump()
     up_score = max_up;
 };
 
-function gravity(sprite)
+function jump_cooloff()
 {
-    if(sprite.y < y_origin) //GRAVITY
+   up_score = 0;
+};
+
+//NEED TO FIX
+function slide()
+{
+    console.log(down_score);
+    down_score++;
+
+    if(down_score < max_down)
     {
-        sprite.y += gravity_power;
+        runner_sprite.y = y_origin + 5;
     }
-    else if(sprite.y == y_origin) //TOUCHED GROUND
+    else
     {
-        up_score = 0;
+        runner_sprite.y = y_origin;
     }
 };
+
+function spawn_obstacle()
+{
+    var choice = getRandomInt(1,4);
+    console.log(choice);
+    var spawn;
+
+    switch (choice) 
+    {
+        case 1:
+            spawn = spawner_ground;
+            break;
+        case 2:
+            spawn = spawner_min;
+            break;
+        case 3:
+            spawn = spawner_med;
+            break;
+        case 4:
+            spawn = spawner_max;
+            break;    
+        default:
+            break;
+    }
+
+    var sprite = kontra.Sprite(
+        {
+            x: 0,
+            y: spawn+8,
+            dx: game_speed,
+            image: kontra.imageAssets.tomato
+        });
+
+    return sprite;
+}
+
+function gravity(sprite)
+{
+    sprite.y += gravity_power;
+};
+
+function getRandomInt(min, max)
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
